c3beabfc1826352c3199ec3a6e55f092ee794899
Tyler Friesen <tylerfriesen@Tylers-MacBook-Pro.local>
2024-03-15 00:00:11 -0600

    fixed Andrew's src files after previous merge broke them

--- src/JS/Pages/main.js
+++ src/JS/Pages/main.js
@@ -3,7 +3,6 @@
 
 const cards = document.querySelectorAll('.qa-card');
 
-console.log(cards);
 cards.forEach(card => {
     card.addEventListener('click', () => {
         card.classList.toggle('open');
