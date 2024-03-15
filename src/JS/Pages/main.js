3f906f2e1f34ecdd2a908718ee404eb39899d0a5
sup3r0001 <forsterandrew15@gmail.com>
2024-03-14 23:37:20 -0700

    Added Footer, fixed bugs with q&a. etc

--- src/JS/Pages/main.js
+++ src/JS/Pages/main.js
@@ -3,9 +3,19 @@
 
 const cards = document.querySelectorAll('.qa-card');
 
-console.log(cards);
 cards.forEach(card => {
     card.addEventListener('click', () => {
+        let cardChild = card.children[1];
         card.classList.toggle('open');
+
+        if (card.classList.contains('open')) {
+            // Set a custom transition duration for opening
+            cardChild.style.transitionDuration = (cardChild.scrollHeight / 200) + 's';
+        } else {
+            // Set the default transition duration for closing
+            cardChild.style.transitionDuration = "0.5s";
+        }        cardChild.style.height = cardChild.style.height ? null : cardChild.scrollHeight + 'px';
+
+        console.log(cardChild.style.transitionDuration);
     })
 })
