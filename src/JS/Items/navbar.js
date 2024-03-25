013cf95e225e792f6016f4cb9ef45c5224e1ba9d
sup3r0001 <forsterandrew15@gmail.com>
2024-03-24 20:19:58 -0700

    Added Query String for filter by and other shop functions.

--- src/JS/Items/navbar.js
+++ src/JS/Items/navbar.js
@@ -60,7 +60,7 @@ function handleEscKey(event) {
 
 const loginBtn = document.getElementById("login-btn");
 const loginDD = document.getElementById("login-dd");
-var login = false;
+var login = true;
 
 $(document).ready(function () {
     if (login == true) {
--- src/JS/Pages/shop.js
+++ src/JS/Pages/shop.js
@@ -1,28 +1,3 @@
-
-var slider = document.getElementById("price-range");
-var val = document.getElementById("price-value");
-
-val.innerHTML = slider.value;
-
-slider.oninput = function () {
-    val.innerHTML = this.value;
-}
-
-let sorter = document.getElementById("sortby");
-let currentSort = 2;
-let sortOptions = ["Price: Low", "Price: High", "Newest", "Oldest"];
-
-sorter.addEventListener("click", function () {
-    if (currentSort < 3) {
-        currentSort++;
-    } else {
-        currentSort = 0;
-    }
-    sorter.innerHTML = sortOptions[currentSort];
-    sorter.value = currentSort;
-});
-
-
 // Product Viewer
 
 let viewBtns = document.querySelectorAll(".product-btn");
@@ -80,4 +55,104 @@ filterBtn.addEventListener("click", function () {
     filter.classList.toggle("active");
 });
 
-// Cart Btn
+// Set URL Query String
+
+function urlQueryString(param, value) {
+    let url = new URL(window.location.href);
+    url.searchParams.set(param, value);
+    window.location.href = url;
+}
+
+//----------------
+// Filter Products
+//----------------
+
+// Slider range
+
+var slider = document.getElementById("price-range");
+var val = document.getElementById("price-value");
+
+val.innerHTML = slider.value;
+
+if (window.location.search.includes("max-price")) {
+    let url = new URL(window.location.href);
+    let maxPrice = url.searchParams.get("max-price");
+    slider.value = maxPrice;
+    val.innerHTML = maxPrice;
+}
+
+slider.oninput = function () {
+    val.innerHTML = this.value;
+};
+
+slider.addEventListener("mouseup", function () {
+        urlQueryString("max-price", this.value);
+});
+
+slider.addEventListener("touchend", function () {
+    urlQueryString("max-price", this.value);
+});
+
+// If slider is being dragged
+
+
+
+
+// Sort By
+
+let sorter = document.getElementById("sortby");
+let sortOptions = ["Price: Low", "Price: High", "Newest", "Oldest"];
+let currentSort;
+
+if (window.location.search.includes("sort")) {
+    let url = new URL(window.location.href);
+    let sort = url.searchParams.get("sort");
+    sorter.value = sort;
+    currentSort = sort;
+    sorter.innerHTML = sortOptions[sort];
+}
+
+sorter.addEventListener("click", function () {
+    if (currentSort < 3) {
+        currentSort++;
+    } else {
+        currentSort = 0;
+    }
+    sorter.innerHTML = sortOptions[currentSort];
+    sorter.value = currentSort;
+
+    urlQueryString("sort", currentSort);
+});
+
+// Checkboxes Inputs
+
+let checkboxes = document.querySelectorAll("#filter input[type='checkbox']");
+
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+            let cbQueryName = checkbox.getAttribute("data-queryName");
+            urlQueryString("searchFor" ,cbQueryName);
+        }
+    });
+});
+
+if (window.location.search.includes("searchFor")) {
+    let url = new URL(window.location.href);
+    let searchFor = url.searchParams.get("searchFor");
+    let checkbox = document.querySelector(`input[data-queryName="${searchFor}"]`);
+    checkbox.checked = true;
+}
+
+
+//-----------
+// Pagination
+//-----------
+
+
+    // Update URL
+
+
+        // Start & End Pages
+
+        
