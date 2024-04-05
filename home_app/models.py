c7d58c786e56450f7b7300165c518708c83c8899
Tyler Friesen <tylerfriesen@Tylers-MacBook-Pro.local>
2024-04-05 14:46:30 -0700

    added image field to Product model

--- home_app/models.py
+++ home_app/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.FileField(upload_to='media/', default="default_img.txt", null=True)
     file = models.FileField(upload_to='media/', default="default_file.txt", null=True)
 
     def __str__(self):
