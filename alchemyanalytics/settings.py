0aea7c11458ba29e0d737536fc11ec587b2ffa44
Tyler Friesen <tylerfriesen@Tylers-MacBook-Pro.local>
2024-04-24 22:57:12 -0700

    ok the production static files should get served now, right?

--- alchemyanalytics/settings.py
+++ alchemyanalytics/settings.py
@@ -186,15 +186,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # For Production only
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # For development only
-'''STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]'''
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
