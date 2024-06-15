a0a152e4d8c4c7e86d923c7966d256a1cde4c1f1
Tyler Friesen <tylerfriesen@Tylers-MacBook-Pro.local>
2024-06-15 11:03:25 -0700

    added leading slash to static url

--- alchemyanalytics/settings.py
+++ alchemyanalytics/settings.py
@@ -178,6 +178,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -197,9 +201,6 @@ if (DEBUG):
     ]
 else:
     # For Production only
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/5.0/howto/static-files/
-    STATIC_URL = 'static/'
     STATIC_ROOT = BASE_DIR / 'staticfiles'
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
